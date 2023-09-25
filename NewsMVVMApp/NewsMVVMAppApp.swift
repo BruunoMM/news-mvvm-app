42c5c70771cc39f476bea22a005c7165293a1145
Bruno Marinho <brunommarinho@hotmail.com>
2023-09-25 17:25:18 -0300

    Initial Commit

---
+++ NewsMVVMApp/NewsMVVMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsMVVMAppApp.swift
+//  NewsMVVMApp
+//
+//  Created by br.marinho on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsMVVMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
